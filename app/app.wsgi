65c95d330900e44b8963c75af118e2557e00d7d6
Raymundo H. Zavala <raymundo@zavala.com.ar>
2023-12-03 10:21:57 -0300

    Primera versión api

---
+++ app/app.wsgi
@@ -0,0 +1,3 @@
+import sys
+sys.path.insert(0, '/home/cacgrupo5/proyecto-backend-codoacodo')
+from aplicacion.app import app as application
---
+++ app/config.py
@@ -0,0 +1,6 @@
+import os
+
+DEBUG = True
+
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://cacgrupo5:Paste2023*@cacgrupo5.mysql.pythonanywhere-services.com/cacgrupo5$default'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
