49fe30042dd4034d9b3ee48975523219e92883dc
Raymundo H. Zavala <raymundo@zavala.com.ar>
2023-12-03 15:47:36 -0300

    agregado de las rutas modificar y eliminar productos

--- app/routes.py
+++ app/routes.py
@@ -16,10 +16,12 @@ def obtener_productos():
     return jsonify([producto.__dict__ for producto in productos])
 
 # Creación de un nuevo producto
+
 @routes.route('/productos', methods=['POST'])
 def crear_producto():
     data = request.get_json()
     nuevo_producto = Producto(
+        id=data['id'],
         categoria=data['categoria'],
         nombre=data['nombre'],
         subcategoria=data['subcategoria'],
@@ -30,3 +32,29 @@ def crear_producto():
     db.session.commit()
     return jsonify({"mensaje": "Producto creado exitosamente"})
 
+# Modificación de un producto
+
+@routes.route('/productos/<int:producto_id>', methods=['PUT'])
+def modificar_producto(producto_id):
+    producto = Producto.query.get_or_404(producto_id)
+    data = request.get_json()
+
+    producto.categoria = data.get('categoria', producto.categoria)
+    producto.nombre = data.get('nombre', producto.nombre)
+    producto.subcategoria = data.get('subcategoria', producto.subcategoria)
+    producto.tamano_cc = data.get('tamano_cc', producto.tamano_cc)
+    producto.precio = data.get('precio', producto.precio)
+
+    db.session.commit()
+    return jsonify({"mensaje": "Producto modificado exitosamente"})
+
+# Eliminación de un producto.
+
+@routes.route('/productos/<int:producto_id>', methods=['DELETE'])
+def eliminar_producto(producto_id):
+    producto = Producto.query.get_or_404(producto_id)
+
+    db.session.delete(producto)
+    db.session.commit()
+
+    return jsonify({"mensaje": "Producto eliminado exitosamente"})
